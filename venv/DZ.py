ed6f8f46eceb02d3f4945465af053f8a8e1ca9e5
Катя <yyffnnfif@gmail.com>
2022-05-16 20:20:28 +0300

    16.05 update

--- venv/DZ.py
+++ venv/DZ.py
@@ -1,39 +1,48 @@
 # ======================================= модуль ДЗ
+import math
 # -----------------------------------------------------------------------
 def get_text_messages(bot, cur_user, message):
     chat_id = message.chat.id
     ms_text = message.text
 
-    if ms_text == "Задание-1":
+    if ms_text == "Факториал":
         dz1(bot, chat_id)
 
-    elif ms_text == "Задание-2":
+    elif ms_text == "Синус":
         dz2(bot, chat_id)
 
-    elif ms_text == "Задание-3":
+    elif ms_text == "Косинус":
         dz3(bot, chat_id)
 
-    elif ms_text == "Задание-4":
+    elif ms_text == "Тангенс":
         dz4(bot, chat_id)
 
-    elif ms_text == "Задание-5":
+    elif ms_text == "Возраст":
         dz5(bot, chat_id)
 
-    elif ms_text == "Задание-6":
+    elif ms_text == "Имя":
         dz6(bot, chat_id)
 
 # -----------------------------------------------------------------------
 def dz1(bot, chat_id):
-    bot.send_message(chat_id, text="ДОДЕЛАТЬ")
+    my_inputInt(bot, chat_id, "Введите число", dz1_ResponseHandler)
+def dz1_ResponseHandler(bot, chat_id, number_int):
+    bot.send_message(chat_id, text=f"Факториал числа {number_int}! \nБудет равен {fac(number_int)}!!!")
 # -----------------------------------------------------------------------
 def dz2(bot, chat_id):
-    bot.send_message(chat_id, text="ДОДЕЛАТЬ")
+    my_inputInt(bot, chat_id, "Введите число", dz2_ResponseHandler)
+def dz2_ResponseHandler(bot, chat_id, number_int):
+    bot.send_message(chat_id, text=f" Cинус числа {number_int}! \nБудет равен {math.sin(number_int)}!!!")
 # -----------------------------------------------------------------------
 def dz3(bot, chat_id):
-    bot.send_message(chat_id, text="ДОДЕЛАТЬ")
+    my_inputInt(bot, chat_id, "Введите число", dz3_ResponseHandler)
+def dz3_ResponseHandler(bot, chat_id, number_int):
+    bot.send_message(chat_id, text=f" Косинус числа {number_int}! \nБудет равен {math.cos(number_int)}!!!")
 # -----------------------------------------------------------------------
 def dz4(bot, chat_id):
-    bot.send_message(chat_id, text="ДОДЕЛАТЬ")
+    my_inputInt(bot, chat_id, "Введите число", dz4_ResponseHandler)
+def dz4_ResponseHandler(bot, chat_id, number_int):
+    bot.send_message(chat_id, text=f" Тангенс числа {number_int}! \nБудет равен {math.tan(number_int)}!!!")
 # -----------------------------------------------------------------------
 def dz5(bot, chat_id):
     my_inputInt(bot, chat_id, "Сколько вам лет?", dz5_ResponseHandler)
@@ -72,4 +81,12 @@ def my_inputInt_SecondPart(message, botQuestion, txtQuestion, ResponseHandler):
                          text="Можно вводить ТОЛЬКО целое число в десятичной системе исчисления (символами от 0 до 9)!\nПопробуйте еще раз...")
         my_inputInt(botQuestion, chat_id, txtQuestion, ResponseHandler)  # это не рекурсия, но очень похоже
         # у нас пара процедур, которые вызывают друг-друга, пока пользователь не введёт корректные данные,
-        # и тогда этот цикл прервётся, и управление перейдёт "наружу", в ResponseHandler
+
+def fac(n):
+    if n == 0:
+        return 1
+    return fac(n-1) * n
+def primer(n):
+    a=random.randint(1,1000)
+    b=random.randint(1,1000)
+    return a+b
